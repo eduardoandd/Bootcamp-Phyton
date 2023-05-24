bfb04416b175a46f6b3e669df7555be6252d06f9
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-05-24 19:04:24 -0300

    operadores aritimeticos

---
+++ tipos_operadores/operadores_aritimeticos.py
@@ -0,0 +1,38 @@
+print(1+1)
+print(1-1)
+print(1*1)
+print(1/1)
+print(10//7)
+
+#Resto da divisão
+print(10%3)
+
+#Exponenciação
+print(2**3)
+
+x = 10-5*2
+print(x)
+
+x  = ((10-5) * 2)
+print(x)
+
+x = (10**2*2)
+print(x)
+
+x= 10 ** (2*2)
+print(x)
+
+x = 10 / 2*4
+print(x)
+
+x = 10**4
+print(x)
+
+produto_1 = 30;
+produto_2 = 20;
+
+print(produto_1 + produto_2)
+print(produto_2 - produto_1)
+print(produto_2 % produto_1)
+print(produto_2 ** produto_1)
+print((produto_2 + produto_2) - produto_1)
