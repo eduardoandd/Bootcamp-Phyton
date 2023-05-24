481f01ad11052121072e5f084aaadf62011fb143
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-05-24 17:19:16 -0300

    variaveis e constantes

---
+++ variaveis_e_constantes.py
@@ -0,0 +1,11 @@
+age = 21
+name= 'Eduardo'
+print(f'Meu nome é {name}, e eu tenho {age} anos')
+
+age, name = (21, 'Eduardo Andrade') 
+print(f'Meu nome é {name}, e eu tenho {age} anos')
+
+limite_saque_diario = 1000
+
+
+STATES = ["SP","RJ","RS"]
