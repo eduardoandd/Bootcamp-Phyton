40c5e36ea79a02e109820a8ed78a2ae6776fd968
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-05-25 16:58:36 -0300

    laços de repetição, break e continue

---
+++ estruturas_repeticao/for.py
@@ -0,0 +1,42 @@
+# texto = input("Informe um texto ")
+# vogais = "AEIOU"
+
+# for letra in texto:
+#     if letra.upper() in vogais:
+#         print(letra, end ="")
+# print()
+print("----------------------------")
+
+#for else
+
+# texto = ""
+# for letra in texto:
+#     if letra.upper() in vogais:
+#         print(letra)
+# else:
+#     print("executar no final do laço")
+    
+print("FOR RANGE")
+
+# for numero in range(0,21,2):
+#     print(numero, end =" ")
+    
+print("------------------------")
+    
+# for numero in range(100):
+#     if numero == 81:
+#         break
+#     print(numero)
+    
+for numero in range(100):
+    if numero == 1:
+        continue
+    print(numero, end=" ")
+
+
+
+
+
+
+    
+    
---
+++ estruturas_repeticao/while.py
@@ -0,0 +1,23 @@
+# opcao = -1
+
+# while opcao != 0:
+#     opcao = int(input("[1] sacar \n[2] Extrato\n[0]sair \n: "))
+    
+#     if opcao ==1:
+#         print("sacando...")
+#         print()
+#         print("Obrigado por utilizar nosso sistema bancario")
+#         break
+#     elif opcao ==2:
+#         print("Exibindo extrato...")
+#         print()
+#         print("Obrigado por utilizar nosso sistema bancario")
+#         break
+#     elif opcao == 0:
+#         break
+#     else:
+#         print("opção invalida")
+
+
+    
+        
